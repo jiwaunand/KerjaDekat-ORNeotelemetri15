374353daf37e6736de82043880e39b22d734b288
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-09-06 16:11:33 +0700

    update docker

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+apt-get update -y
+apt-get install -y podman podman-compose cockpit cockpit-podman
+mkdir -p /etc/containers
+echo 'unqualified-search-registries = ["docker.io", "quay.io"]' > /etc/containers/registries.conf
+podman pod create --name mainpod
+podman-compose up -d --build
+systemctl enable --now cockpit.socket
