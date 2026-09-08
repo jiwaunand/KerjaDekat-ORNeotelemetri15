640b0ffe4103967b37f70b9233a7108dbc0c884b
mutiaraaisyshofi <mutiaraaisyshofii@gmail.com>
2026-09-08 23:27:24 +0700

    fix database config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,15 +3,9 @@ const { Pool } = require("pg");
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-<<<<<<< HEAD
   database: process.env.POSTGRES_DB,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD
-=======
-  database: process.env.POSTGRES_DB || process.env.DB_NAME,
-  user: process.env.POSTGRES_USER || process.env.DB_USER,
-  password: process.env.POSTGRES_PASSWORD || process.env.DB_PASSWORD
->>>>>>> 856d559 (update backend upload pagination swagger)
 });
 
 const connectDB = async () => {
@@ -24,12 +18,7 @@ const connectDB = async () => {
   }
 };
 
-<<<<<<< HEAD
 module.exports = {
   pool,
   connectDB
 };
-=======
-module.exports = pool;
-module.exports.connectDB = connectDB;
->>>>>>> 856d559 (update backend upload pagination swagger)
